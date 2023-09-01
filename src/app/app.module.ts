5c167b52040fd97acb246a74667164e36ecb5be9
Adel Benyahia <a.benyahia6993@gmail.com>
2023-09-01 13:47:43 +0200

    Creation Module header + routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderModule } from './header/header.module';
+import { HomeComponent } from './pages/home/home.component';
+import { ProductComponent } from './pages/product/product.component';
+import { GaleryComponent } from './pages/galery/galery.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ProductComponent,
+    GaleryComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HeaderModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/header/header.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MenuDesktopComponent } from './menu-desktop/menu-desktop.component';
+import { MenuMobileComponent } from './menu-mobile/menu-mobile.component';
+import { HeaderComponent } from './header.component';
+import { LoginButtonComponent } from './login-button/login-button.component';
+import { RouterModule } from '@angular/router';
+
+@NgModule({
+  declarations: [MenuDesktopComponent, MenuMobileComponent, HeaderComponent, LoginButtonComponent],
+
+  imports: [CommonModule,RouterModule],
+  exports: [HeaderComponent],
+  bootstrap: [HeaderComponent],
+})
+export class HeaderModule {
+
+  
+}
